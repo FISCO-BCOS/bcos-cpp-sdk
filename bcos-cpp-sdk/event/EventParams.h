934574fbeab7dd195d3d6dcfb6343af2746797c5
octopus <912554887@qq.com>
2021-09-06 11:16:25 +0800

    modify event params

--- bcos-cpp-sdk/event/EventParams.h
+++ bcos-cpp-sdk/event/EventParams.h
@@ -13,21 +13,14 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
-<<<<<<< HEAD
- * @file EvenPushParams.h
-=======
  * @file EvenParams.h
->>>>>>> 8e0a31c (add event impl)
  * @author: octopus
  * @date 2021-09-01
  */
 
 #pragma once
 #include <bcos-cpp-sdk/event/Common.h>
-<<<<<<< HEAD
 #include <bcos-framework/interfaces/protocol/ProtocolTypeDef.h>
-=======
->>>>>>> 8e0a31c (add event impl)
 #include <string>
 #include <vector>
 
@@ -37,41 +30,23 @@ namespace cppsdk
 {
 namespace event
 {
-<<<<<<< HEAD
-class EventPushParams
-{
-public:
-    using Ptr = std::shared_ptr<EventPushParams>;
-
-public:
-    bcos::protocol::BlockNumber fromBlock() const { return m_fromBlock; }
-    bcos::protocol::BlockNumber toBlock() const { return m_toBlock; }
-    const std::vector<std::string>& addresses() const { return m_addresses; }
-    std::vector<std::string>& addresses() { return m_addresses; }
-    const std::vector<std::vector<std::string>>& topics() const { return m_topics; }
-    std::vector<std::vector<std::string>>& topics() { return m_topics; }
-
-    void setFromBlock(bcos::protocol::BlockNumber _fromBlock) { m_fromBlock = _fromBlock; }
-    void setToBlock(bcos::protocol::BlockNumber _toBlock) { m_toBlock = _toBlock; }
-=======
 class EventParams
 {
 public:
     using Ptr = std::shared_ptr<EventParams>;
 
 public:
-    std::string fromBlock() const { return m_fromBlock; }
-    std::string toBlock() const { return m_toBlock; }
+    int64_t fromBlock() const { return m_fromBlock; }
+    int64_t toBlock() const { return m_toBlock; }
     std::string group() const { return m_group; }
     const std::vector<std::string>& addresses() const { return m_addresses; }
     std::vector<std::string>& addresses() { return m_addresses; }
-    const std::vector<std::vector<std::string> >& topics() const { return m_topics; }
-    std::vector<std::vector<std::string> >& topics() { return m_topics; }
+    const std::vector<std::vector<std::string>>& topics() const { return m_topics; }
+    std::vector<std::vector<std::string>>& topics() { return m_topics; }
 
     void setGroup(const std::string& _group) { m_group = _group; }
-    void setFromBlock(const std::string& _fromBlock) { m_fromBlock = _fromBlock; }
-    void setToBlock(const std::string& _toBlock) { m_toBlock = _toBlock; }
->>>>>>> 8e0a31c (add event impl)
+    void setFromBlock(int64_t _fromBlock) { m_fromBlock = _fromBlock; }
+    void setToBlock(int64_t _toBlock) { m_toBlock = _toBlock; }
     void addAddress(const std::string& _address) { m_addresses.push_back(_address); }
     bool addTopic(std::size_t _index, const std::string& _topic)
     {
@@ -85,23 +60,18 @@ public:
         return true;
     }
 
-<<<<<<< HEAD
 private:
     bcos::protocol::BlockNumber m_fromBlock = -1;
     bcos::protocol::BlockNumber m_toBlock = -1;
     std::vector<std::string> m_addresses;
     std::vector<std::vector<std::string>> m_topics;
-=======
-    bool verify();
-    std::string toJson();
 
 private:
-    std::string m_fromBlock;
-    std::string m_toBlock;
+    int64_t m_fromBlock = -1;
+    int64_t m_toBlock = -1;
     std::vector<std::string> m_addresses;
-    std::vector<std::vector<std::string> > m_topics;
+    std::vector<std::vector<std::string>> m_topics;
     std::string m_group;
->>>>>>> 8e0a31c (add event impl)
 };
 
 }  // namespace event
