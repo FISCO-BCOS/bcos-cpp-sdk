3847fd3b5d1762708de76e1cb2a0c804f5fe8527
ywy2090 <912554887@qq.com>
2022-02-10 00:21:47 +0800

    Feature idle (#80)
    * update bcos-boostssl (#78)
    
    * update bcos-boostssl (#79)

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -29,8 +29,8 @@ hunter_config(bcos-crypto VERSION 3.0.0-rc3-local
 
 hunter_config(bcos-boostssl
 	VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/685288e31c45ab983fc68b789ba81838948e2991.tar.gz
-	SHA1 ba076ca3770b5a59e58dcc1772c99718d29f3854
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/16ce8a9f1125b86b18e888bcc1267d0ec6bf2d38.tar.gz
+	SHA1 e887ef537a387dcb122f5fc30ac91b5889dac412
 )
 
 
