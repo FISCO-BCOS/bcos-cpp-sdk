48bc8885e23f56412dd6f8d9e69af82ec7fa3087
lucasli <410567249@qq.com>
2022-04-28 20:16:18 +0800

    add flag message that need to check respond packet in wssession

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -28,6 +28,6 @@ hunter_config(bcos-crypto VERSION 1.0.0-rc2-local
 )
 
 hunter_config(bcos-boostssl VERSION 1.0.0-rc3-local
-	URL https://github.com/LucasLi1024/bcos-boostssl/archive/8daab0c4aca15e8d5606f79c05ab57e1e4d891a6.tar.gz
-	SHA1 2913721afd8393b66323a1db27dfc092d204fd8f
+	URL https://github.com/LucasLi1024/bcos-boostssl/archive/b7dfe34bab17f405d033072e1880065b8efbc8f8.tar.gz
+	SHA1 76152c6664de5616fd3951a5d3296996cf32e8d7
 )
