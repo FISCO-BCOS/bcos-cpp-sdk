8266c4087008408c2dd141115b643576ce97a4e0
ywy2090 <912554887@qq.com>
2022-12-26 14:59:49 +0800

    add extraData field for transaction (#149)

--- bcos-cpp-sdk/utilities/tx/Transaction.h
+++ bcos-cpp-sdk/utilities/tx/Transaction.h
@@ -25,8 +25,8 @@ struct TransactionData : public tars::TarsStructBase
 public:
     static std::string className() { return "bcostars.TransactionData"; }
     static std::string MD5() { return "ea41d47be6b852a5c3edcfe7a805be85"; }
-    TransactionData() { resetDefautlt(); }
-    void resetDefautlt()
+    TransactionData() { resetDefault(); }
+    void resetDefault()
     {
         version = 0;
         chainID = "";
@@ -58,7 +58,7 @@ public:
     template <typename ReaderT>
     void readFrom(tars::TarsInputStream<ReaderT>& _is)
     {
-        resetDefautlt();
+        resetDefault();
         _is.read(version, 1, true);
         _is.read(chainID, 2, true);
         _is.read(groupID, 3, true);
@@ -84,8 +84,8 @@ public:
     std::string writeToJsonString() const { return tars::TC_Json::writeValue(writeToJson()); }
     void readFromJson(const tars::JsonValuePtr& p, bool isRequire = true)
     {
-        resetDefautlt();
-        if (nullptr == p.get() || p->getType() != tars::eJsonTypeObj)
+        resetDefault();
+        if (NULL == p.get() || p->getType() != tars::eJsonTypeObj)
         {
             char s[128];
             snprintf(s, sizeof(s), "read 'struct' type mismatch, get type: %d.",
@@ -148,7 +148,7 @@ public:
                 // encode blockLimit
                 int64_t networkBlockLimit = boost::endian::native_to_big((int64_t)blockLimit);
                 hasher.update(bcos::bytesConstRef((bcos::byte*)(&networkBlockLimit),
-                                           sizeof(networkBlockLimit) / sizeof(uint8_t)));
+                    sizeof(networkBlockLimit) / sizeof(uint8_t)));
                 // encode nonce
                 hasher.update(bcos::bytesConstRef((bcos::byte*)nonce.data(), nonce.size()));
                 // encode to
@@ -202,17 +202,17 @@ struct Transaction : public tars::TarsStructBase
 {
 public:
     static std::string className() { return "bcostars.Transaction"; }
-    static std::string MD5() { return "ad46f3dbb0da87bdc5ed991eb2a8caff"; }
-    Transaction() { resetDefautlt(); }
-    void resetDefautlt()
+    static std::string MD5() { return "1e037304f04d104276ebd5b7c4aebdea"; }
+    Transaction() { resetDefault(); }
+    void resetDefault()
     {
-        data.resetDefautlt();
+        data.resetDefault();
         dataHash.clear();
         signature.clear();
         importTime = 0;
         attribute = 0;
-        source = "";
         sender.clear();
+        extraData = "";
     }
     template <typename WriterT>
     void writeTo(tars::TarsOutputStream<WriterT>& _os) const
@@ -234,26 +234,26 @@ public:
         {
             _os.write(attribute, 5);
         }
-        if (source != "")
-        {
-            _os.write(source, 6);
-        }
         if (sender.size() > 0)
         {
             _os.write(sender, 7);
+        }
+        if (extraData != "")
+        {
+            _os.write(extraData, 8);
         }
     }
     template <typename ReaderT>
     void readFrom(tars::TarsInputStream<ReaderT>& _is)
     {
-        resetDefautlt();
+        resetDefault();
         _is.read(data, 1, false);
         _is.read(dataHash, 2, false);
         _is.read(signature, 3, false);
         _is.read(importTime, 4, false);
         _is.read(attribute, 5, false);
-        _is.read(source, 6, false);
         _is.read(sender, 7, false);
+        _is.read(extraData, 8, false);
     }
     tars::JsonValueObjPtr writeToJson() const
     {
@@ -263,14 +263,14 @@ public:
         p->value["signature"] = tars::JsonOutput::writeJson(signature);
         p->value["importTime"] = tars::JsonOutput::writeJson(importTime);
         p->value["attribute"] = tars::JsonOutput::writeJson(attribute);
-        p->value["source"] = tars::JsonOutput::writeJson(source);
         p->value["sender"] = tars::JsonOutput::writeJson(sender);
+        p->value["extraData"] = tars::JsonOutput::writeJson(extraData);
         return p;
     }
     std::string writeToJsonString() const { return tars::TC_Json::writeValue(writeToJson()); }
     void readFromJson(const tars::JsonValuePtr& p, bool isRequire = true)
     {
-        resetDefautlt();
+        resetDefault();
         if (NULL == p.get() || p->getType() != tars::eJsonTypeObj)
         {
             char s[128];
@@ -284,8 +284,8 @@ public:
         tars::JsonInput::readJson(signature, pObj->value["signature"], false);
         tars::JsonInput::readJson(importTime, pObj->value["importTime"], false);
         tars::JsonInput::readJson(attribute, pObj->value["attribute"], false);
-        tars::JsonInput::readJson(source, pObj->value["source"], false);
         tars::JsonInput::readJson(sender, pObj->value["sender"], false);
+        tars::JsonInput::readJson(extraData, pObj->value["extraData"], false);
     }
     void readFromJsonString(const std::string& str) { readFromJson(tars::TC_Json::getValue(str)); }
     std::ostream& display(std::ostream& _os, int _level = 0) const
@@ -296,8 +296,8 @@ public:
         _ds.display(signature, "signature");
         _ds.display(importTime, "importTime");
         _ds.display(attribute, "attribute");
-        _ds.display(source, "source");
         _ds.display(sender, "sender");
+        _ds.display(extraData, "extraData");
         return _os;
     }
     std::ostream& displaySimple(std::ostream& _os, int _level = 0) const
@@ -308,8 +308,8 @@ public:
         _ds.displaySimple(signature, true);
         _ds.displaySimple(importTime, true);
         _ds.displaySimple(attribute, true);
-        _ds.displaySimple(source, true);
-        _ds.displaySimple(sender, false);
+        _ds.displaySimple(sender, true);
+        _ds.displaySimple(extraData, false);
         return _os;
     }
 
@@ -319,14 +319,14 @@ public:
     std::vector<tars::Char> signature;
     tars::Int64 importTime;
     tars::Int32 attribute;
-    std::string source;
     std::vector<tars::Char> sender;
+    std::string extraData;
 };
 inline bool operator==(const Transaction& l, const Transaction& r)
 {
     return l.data == r.data && l.dataHash == r.dataHash && l.signature == r.signature &&
-           l.importTime == r.importTime && l.attribute == r.attribute && l.source == r.source &&
-           l.sender == r.sender;
+           l.importTime == r.importTime && l.attribute == r.attribute && l.sender == r.sender &&
+           l.extraData == r.extraData;
 }
 inline bool operator!=(const Transaction& l, const Transaction& r)
 {
