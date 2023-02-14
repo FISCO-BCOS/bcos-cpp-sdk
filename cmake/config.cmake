518eb7f27b202e89cc2c478661a720d12e53bac4
bxq2011hust <bxq2011hust@qq.com>
2023-02-14 16:51:38 +0800

    fix windows compile

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,11 +1,11 @@
 hunter_config(
-    Boost VERSION "1.76.0-local"
+    Boost VERSION "1.79.0"
     URL
-    "https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/FISCO-BCOS/FISCO-BCOS/deps/boost_1_76_0.tar.bz2
-    https://downloads.sourceforge.net/project/boost/boost/1.76.0/source/boost_1_76_0.tar.bz2
-    https://nchc.dl.sourceforge.net/project/boost/boost/1.76.0/boost_1_76_0.tar.bz2"
+    "https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/FISCO-BCOS/FISCO-BCOS/deps/boost_1_79_0.tar.bz2
+    https://downloads.sourceforge.net/project/boost/boost/1.79.0/source/boost_1_79_0.tar.bz2
+    https://nchc.dl.sourceforge.net/project/boost/boost/1.79.0/boost_1_79_0.tar.bz2"
     SHA1
-    8064156508312dde1d834fec3dca9b11006555b6
+    31209dcff292bd6a64e5e08ceb3ce44a33615dc0
     CMAKE_ARGS
     CONFIG_MACRO=BOOST_UUID_RANDOM_PROVIDER_FORCE_POSIX
 )
